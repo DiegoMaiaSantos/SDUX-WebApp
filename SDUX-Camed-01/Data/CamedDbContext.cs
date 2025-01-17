a8685d957106ca11da8a3a8c8de0288df3183c6c
Diego Santos <diegom.santos03@gmail.com>
2025-01-17 19:08:57 -0300

    chore: projeto criado e finalizado, validacoes realizadas com sucesso.

---
+++ SDUX-Camed-01/Data/CamedDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SDUX_Camed_01.Models;
+
+namespace SDUX_Camed_01.Data
+{
+    public class CamedDbContext : DbContext
+    {
+        public CamedDbContext(DbContextOptions<CamedDbContext> options) : base(options) { }
+
+        public DbSet<UserModel> Users { get; set; }
+    }
+}
---
+++ SDUX-Camed-01/Models/UserModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SDUX_Camed_01.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Nome { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public DateTime DataNascimento { get; set; }
+    }
+}
