f23de63f31a001e5e2edf94921450e3fd934ced1
Diego Santos <diegom.santos03@gmail.com>
2025-01-20 18:24:53 -0300

    chore: criado o mapeamento das entidades e models e realizado configuracoes na program para o funcionamento do mapper e a injecao de dependencias.

---
+++ SDUX-Camed-API-02/Mapper/ClienteMappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using SDUX_Camed_API_02.Entities;
+using SDUX_Camed_API_02.Models;
+
+namespace SDUX_Camed_API_02.Mapper
+{
+    public class ClienteMappingProfile : Profile
+    {
+        public ClienteMappingProfile()
+        {
+            CreateMap<ClienteModel, Cliente>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore());
+
+            CreateMap<Cliente, ClienteModel>();
+        }
+    }
+}
--- SDUX-Camed-API-02/Program.cs
+++ SDUX-Camed-API-02/Program.cs
@@ -1,5 +1,10 @@
 using Microsoft.EntityFrameworkCore;
 using SDUX_Camed_API_02.Data;
+using SDUX_Camed_API_02.Interfaces.Repositories;
+using SDUX_Camed_API_02.Interfaces.Services;
+using SDUX_Camed_API_02.Mapper;
+using SDUX_Camed_API_02.Repositories;
+using SDUX_Camed_API_02.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,7 +12,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<CamedDbContext>(options => 
     options.UseSqlServer(builder.Configuration.GetConnectionString("CamedConnection")));
 
+// Add AutoMApperProfile
+builder.Services.AddAutoMapper(typeof(ClienteMappingProfile));
+
 // Add services to the container.
+builder.Services.AddScoped<IClienteService, ClienteService>();
+
+// Add repositories to the container.
+builder.Services.AddScoped<IClienteRepository, ClienteRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
