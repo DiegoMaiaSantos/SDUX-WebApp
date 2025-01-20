13610b535fa0dfabb26dd1c65ddda20dfbb68c1b
Diego Santos <diegom.santos03@gmail.com>
2025-01-20 18:29:08 -0300

    feat: criado projeto base da webApp

---
+++ SDUX-Camed-04/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SDUX_Camed_04.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
